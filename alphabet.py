164da863b47dd65c665192fce68ba6ec3a706771
Misa123131 <160743421+Misa123131@users.noreply.github.com>
2024-02-22 17:05:22 +0700

    Add files via upload

---
+++ alphabet.py
@@ -0,0 +1,255 @@
+    # А and а
+def big_A():
+    return 'А'
+
+def small_A():
+    return 'а'
+
+
+    # Б и б
+def big_B():
+    return 'Б'
+
+def small_b():
+    return 'б'
+
+
+    # В и в
+def big_V():
+    return 'В'
+
+def small_v():
+    return 'в'
+
+
+    # Г и г
+def big_G():
+    return 'Г'
+
+def small_g():
+    return'г'
+
+
+    # Д и д
+def big_D():
+    return 'Д'
+
+def small_d():
+    return'д'
+
+
+    # Е и е
+def big_E():
+    return 'Е'
+
+def small_e():
+    return'е'
+
+
+    # Ё и ё
+def big_Yo():
+    return 'Ё'
+
+def small_yo():
+    return'ё'
+
+
+    # Ж и ж
+def big_Zh():
+    return 'Ж'
+
+def small_zh():
+    return'ж'   
+
+
+    # З и з
+def big_Z():
+    return 'З'
+
+def small_z():
+    return'з'    
+
+
+    # И и и
+def big_Li():
+    return 'И'
+
+def small_li():
+    return'и'    
+
+
+    # Й и Й
+def big_Yy():
+    return 'Й'
+
+def small_yy():
+    return'й'   
+
+
+    # К и к
+def big_K():
+    return 'К'
+
+def small_k():
+    return'к'   
+
+    # Л и л
+def big_L():
+    return 'Л'
+
+def small_l():
+    return'л'  
+
+
+    # М и м
+def big_M():
+    return 'М'
+
+def small_m():
+    return'м' 
+
+
+    # Н и н
+def big_N():
+    return 'Н'
+
+def small_n():
+    return'н'  
+
+
+    # О и о
+def big_O():
+    return 'О'
+
+def small_o():
+    return'о' 
+
+
+    # П и п
+def big_P():
+    return 'П'
+
+def small_p():
+    return'п' 
+
+
+    # Р и р
+def big_R():
+    return 'Р'
+
+def small_r():
+    return'р'  
+
+
+    # С и с
+def big_S():
+    return 'С'
+
+def small_s():
+    return'с' 
+
+
+    # Т и т
+def big_T():
+    return 'Т'
+
+def small_t():
+    return'т'  
+
+
+    # У и у
+def big_U():
+    return 'У'
+
+def small_u():
+    return'у'  
+
+
+    # Ф и ф
+def big_F():
+    return 'Ф'
+
+def small_f():
+    return'ф' 
+
+
+    # Х и х
+def big_Kh():
+    return 'Х'
+
+def small_kh():
+    return'х' 
+
+
+    # Ц и ц
+def big_Ts():
+    return 'Ц'
+
+def small_ts():
+    return'ц' 
+
+
+    # Ч и ч
+def big_Ch():
+    return 'Ч'
+
+def small_ch():
+    return'ч' 
+
+
+    # Ш и ш
+def big_Sh():
+    return 'Ш'
+
+def small_sh():
+    return'ш'   
+
+
+    # Щ и щ
+def big_Shch():
+    return 'Щ'
+
+def small_shch():
+    return'щ'  
+
+
+    # Ъ и ъ
+def big_solid_sign():
+    return 'Ъ'
+
+def small_solid_sign():
+    return'ъ'  
+
+
+    # Ь и ь
+def big_soft_sign():
+    return 'Ь'
+
+def small_soft_sign():
+    return'ь' 
+
+
+    # Э и э
+def big_EE():
+    return 'Э'
+
+def small_ee():
+    return'э' 
+
+
+    # Ю и ю
+def big_Y():
+    return 'Ю'
+
+def small_y():
+    return'ю' 
+
+
+    # Я и я
+def big_Ya():
+    return 'Я'
+
+def small_ya():
+    return'я'    
+
+
